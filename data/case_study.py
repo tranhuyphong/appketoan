f40087dde67067e0c528ee4963d1dda6d0faa3e3
tranhuyphong <tranhuyphong2102@gmail.com>
2026-04-21 15:31:23 +0700

    Create case_study.py

---
+++ data/case_study.py
@@ -0,0 +1,18 @@
+case_studies = [
+
+    {
+        "title": "Công ty ABC - Tháng 1",
+
+        "transactions": [
+            "Góp vốn 100 triệu tiền mặt",
+            "Mua hàng 20 triệu tiền mặt",
+            "Bán hàng thu tiền 30 triệu"
+        ],
+
+        "answers": [
+            "Nợ 111 / Có 411",
+            "Nợ 156 / Có 111",
+            "Nợ 111 / Có 511"
+        ]
+    }
+]
