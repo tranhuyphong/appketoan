3421a2f7ac28033cb7f4ff6e2980f80ce051e8e5
tranhuyphong <tranhuyphong2102@gmail.com>
2026-04-21 16:57:31 +0700

    Create finance_data.py

---
+++ data/finance_data.py
@@ -0,0 +1,7 @@
+transactions = [
+    {"type": "revenue", "amount": 5000},
+    {"type": "expense", "amount": 2000},
+    {"type": "expense", "amount": 1500},
+    {"type": "revenue", "amount": 7000},
+    {"type": "expense", "amount": 1000},
+]
