e7d78c70a09af98d32090df8020dab073b4d0103
tranhuyphong <tranhuyphong2102@gmail.com>
2026-04-21 13:50:27 +0700

    Update boss_ai.py

--- engine/boss_ai.py
+++ engine/boss_ai.py
@@ -1,6 +1,9 @@
 import streamlit as st
 from openai import OpenAI
 
+import streamlit as st
+from openai import OpenAI
+
 client = OpenAI(api_key=st.secrets["OPENAI_API_KEY"])
 
 def boss_msg(task):
