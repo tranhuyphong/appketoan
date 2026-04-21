204ce03ca442e008c0fd941b4c1c2a3f9be767f8
tranhuyphong <tranhuyphong2102@gmail.com>
2026-04-21 14:58:02 +0700

    Create question_bank.py

---
+++ data/question_bank.py
@@ -0,0 +1,38 @@
+question_bank = [
+
+    {
+        "id": 1,
+        "skill": "phương trình kế toán",
+        "level": "easy",
+
+        "question": "Tài sản = ?",
+
+        "options": [
+            "Chi phí",
+            "Nợ + VCSH",
+            "Doanh thu"
+        ],
+
+        "correct": 1,
+
+        "explain": "Tài sản luôn bằng Nợ phải trả + Vốn chủ sở hữu"
+    },
+
+    {
+        "id": 2,
+        "skill": "định khoản",
+        "level": "medium",
+
+        "question": "Góp vốn bằng tiền mặt định khoản thế nào?",
+
+        "options": [
+            "Nợ 111 / Có 511",
+            "Nợ 111 / Có 411",
+            "Nợ 112 / Có 411"
+        ],
+
+        "correct": 1,
+
+        "explain": "Góp vốn làm tăng tiền và vốn chủ"
+    }
+]
