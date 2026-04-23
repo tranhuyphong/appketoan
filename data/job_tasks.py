cb33cce18805aa761120d0945d98f387335c9e10
tranhuyphong <tranhuyphong2102@gmail.com>
2026-04-23 15:43:00 +0700

    Update and rename jobs.py to job_tasks.py

---
+++ data/job_tasks.py
@@ -0,0 +1,47 @@
+job_tasks = [
+    {
+        "level": 1,
+        "title": "Ghi nhận mua hàng",
+        "question": "Công ty mua hàng 10 triệu, chưa trả tiền?",
+        "options": [
+            "Nợ 156 / Có 331",
+            "Nợ 111 / Có 156",
+            "Nợ 331 / Có 156",
+            "Nợ 156 / Có 111"
+        ],
+        "correct": 0,
+        "salary": 20,
+        "penalty": -10,
+        "time": 20
+    },
+    {
+        "level": 1,
+        "title": "Thu tiền khách",
+        "question": "Khách trả 5 triệu?",
+        "options": [
+            "Nợ 131 / Có 111",
+            "Nợ 111 / Có 131",
+            "Nợ 511 / Có 111",
+            "Nợ 111 / Có 511"
+        ],
+        "correct": 1,
+        "salary": 20,
+        "penalty": -10,
+        "time": 15
+    },
+    {
+        "level": 2,
+        "title": "Khấu hao",
+        "question": "Khấu hao 2 triệu?",
+        "options": [
+            "Nợ 642 / Có 214",
+            "Nợ 214 / Có 642",
+            "Nợ 111 / Có 214",
+            "Nợ 214 / Có 111"
+        ],
+        "correct": 0,
+        "salary": 40,
+        "penalty": -20,
+        "time": 15
+    }
+]
